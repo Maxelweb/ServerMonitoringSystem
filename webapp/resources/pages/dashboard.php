b5567be2fb694c9f0bcead7c79060f3880488ce1
Maxelweb <Maxelweb@users.noreply.github.com>
2019-08-13 12:08:33 +0200

    Colors for widget

--- webapp/resources/pages/dashboard.php
+++ webapp/resources/pages/dashboard.php
@@ -8,16 +8,16 @@
 		<div class="title"> <i class="fas fa-lg fa-thermometer-half"></i> Room Temperature</div>	
 		<div class="content">24° C</div>
 	</div>
-	<div class="widget">
+	<div class="widget widget-success">
 		<div class="title"> <i class="fas fa-lg fa-tint"></i> Room Humidity</div>	
 		<div class="content">80%</div>
 	</div>
-	<div class="widget">
+	<div class="widget widget-danger">
 		<div class="title"> <i class="fas fa-lg fa-door-closed"></i> Door</div>	
 		<div class="content">Closed</div>
 	</div>
 
-	<div class="widget">
+	<div class="widget widget-warning">
 		<div class="title"> <i class="far fa-lg fa-eye"></i> Presence</div>	
 		<div class="content">None</div>
 	</div>
